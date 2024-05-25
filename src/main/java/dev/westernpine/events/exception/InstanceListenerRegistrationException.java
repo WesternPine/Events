d18023691f410181c6be9b04bcdce90a397b83f7
WesternPine <tyler@westernpine.dev>
2024-05-24 22:54:04 -0400

    Patch Fixed exception message.

--- src/main/java/dev/westernpine/events/exception/InstanceListenerRegistrationException.java
+++ src/main/java/dev/westernpine/events/exception/InstanceListenerRegistrationException.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 public class InstanceListenerRegistrationException extends RuntimeException {
 
     public InstanceListenerRegistrationException(Object instance, Method method) {
-        super("Method %s was registered with an incorrect instance, which requires an instance of class %s.".formatted(method.getName(), method.getClass().getName())
-        + (Objects.isNull(instance) ? "(Instance was null.)" : ""));
+        super("Method %s was registered with an incorrect instance, which requires an instance of class %s. ".formatted(method.getName(), method.getDeclaringClass().getName())
+        + (Objects.isNull(instance) ? "(Instance was null.)" : instance.getClass().getName() + " Was provided instead."));
     }
 }
